ef3686e52fc909cfbd73bda0f3ea52de04712896
CodyClark07 <codyclark2010@hotmail.com>
2020-08-06 21:08:45 -0600

    ..

--- clientsrc/src/views/Boards.vue
+++ clientsrc/src/views/Boards.vue
@@ -46,7 +46,7 @@
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title">Edit "{{board.title}}"</h5>
+                <h5 class="modal-title text-capitalize..">Edit "{{board.title}}"</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
